3286e2bca2ac00de06625b2e94b115bc2b19115a
Costas Papastathis <papastathiscr@gmail.com>
2024-08-21 14:06:48 +0300

    another commit

--- server.js
+++ server.js
@@ -1,14 +1,36 @@
 const { createServer } = require("http");
 const PORT = process.env.PORT || 3000;
 
+const magicConchShellAnswers = [
+  "Maybe someday",
+  "Nothing",
+  "Neither",
+  "I don't think so",
+  "No",
+  "Yes",
+  "Try asking again",
+  "You cannot get to the top by sitting on your bottom",
+  "I see a new sauce in your future",
+  "Ask next time",
+  "Follow the seahorse",
+];
+
 const server = createServer((req, res) => {
-  if (req.url.match(/\/hello\/node/) && req.method === "GET") {
+  if (req.url.match(/\/advice/) && req.method === "GET") {
+    Math.floor(Math.random() * magicConchShellAnswers.length);
+
+    const advice =
+      magicConchShellAnswers[
+        Math.floor(Math.random() * magicConchShellAnswers.length)
+      ];
+
     res.writeHead(200, { "Content-Type": "application/json" });
     res.end(
       JSON.stringify({
-        data: "hello",
+        data: advice,
       })
     );
+    res.end();
   } else if (req.url.match(/\/health/) && req.method === "GET") {
     res.writeHead(200);
     res.end();
@@ -18,9 +40,4 @@ const server = createServer((req, res) => {
   }
 });
 
-function handle(signal) {
-  console.log(`*^!@4=> Received event: ${signal}`);
-}
-process.on("SIGHUP", handle);
-
-server.listen(PORT, () => console.log(`Server running on port:  ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
